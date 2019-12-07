bff2d27bd3fb340a2b489162d1ff566333b21dd4
HoangTranUnion <56041330+HoangTranUnion@users.noreply.github.com>
2019-12-07 18:30:02 +0700

    Add files via upload

---
+++ BackgroundCode/DownloadModes.py
@@ -0,0 +1,53 @@
+from BackgroundCode.DownloadPics import DownloadMultipleTags
+
+
+def manual_download():
+    '''
+    Manually let the user pick and download the tags
+    :return:
+    '''
+    def isContinue(answer):
+        '''
+        Checks if more tag needs to be added
+        :param answer: An answer
+        :return: True if more is needed, False otherwise
+        '''
+        if answer.lower() == "y" or answer.lower() == "yes":
+            continue_adding = True
+        elif answer.lower() == "n" or answer.lower() == "yes":
+            continue_adding = False
+        return continue_adding
+
+
+    print("Welcome to Danbooru Downloader!")
+    user_input_list = []
+    continue_input = True
+    acceptable_boolean_answers = ["Y", "N", "yes", "no", "Yes", "No", "YEs", "NO", 'YeS', "nO", "yES", "yeS", "yEs", "YES",
+                                  "y", "n"]
+    while continue_input:
+        user_input = input("Please enter your desired tag on Danbooru: ")
+        user_input_list.append(user_input)
+        answer = input("Do you want to search for another tag? Y/N: ")
+        if answer in acceptable_boolean_answers:
+            continue_input = isContinue(answer)
+        else:
+            satisfactory_format = False
+            while not satisfactory_format:
+                print("Please enter Y or N.")
+                answer = input("Do you want to search for another tag? Y/N: ")
+                if answer in acceptable_boolean_answers:
+                    satisfactory_format = True
+                else:
+                    satisfactory_format = False
+            continue_input = isContinue(answer)
+
+    DownloadMultipleTags(user_input_list, "prefered\\download\\location").initiate_download()
+
+
+def auto_download():
+    '''
+    Automatically downloads pictures with a given list of tags.
+    '''
+    list_of_tags = ['mash_kyrielight','mysterious_heroine_x_(alter)','hk416_(girls_frontline)',
+                    'osakabe-hime_(fate/grand_order)','sesshouin_kiara']
+    DownloadMultipleTags(list_of_tags,"prefered\\download\\location").initiate_download()
--- BackgroundCode/DownloadPics.py
+++ BackgroundCode/DownloadPics.py
@@ -10,14 +10,16 @@ from BackgroundCode.DumpToPickle import DumpToPickle
 
 
 ANON_SEARCH_LIMIT = 1000
+MAX_POST_PER_PAGE = 20
+
+
+class FalseTag(Exception):
+    pass
 
 
 class DownloadDanbooruPics:
-    def __init__(self,tag):
-        '''
-
-        :param tag:
-        '''
+    def __init__(self,tag, location):
+        self.__location = location
         self.__tag = tag
         self.__isAltered = False
         for char in tag:
@@ -26,17 +28,35 @@ class DownloadDanbooruPics:
                 self.__isAltered = True
         if self.__isAltered:
             self.__temp_tag = tag
-            self.__JSON_path = os.path.join(settings.JSON_FILES_PATH, str(self.__temp_tag + ".json"))
             self.__pickle_path = os.path.join(settings.PICKLE_FILES_PATH, str(self.__temp_tag))
             self.__page_counter_path = os.path.join(settings.TAG_PAGE_COUNTERS_PATH, str(self.__temp_tag + ".txt"))
             self.__tag_run_counter_path = os.path.join(settings.TAG_RUN_COUNTER, str(self.__temp_tag + ".txt"))
         else:
-            self.__JSON_path = os.path.join(settings.JSON_FILES_PATH, str(self.__tag + ".json"))
             self.__pickle_path = os.path.join(settings.PICKLE_FILES_PATH, str(self.__tag))
             self.__page_counter_path = os.path.join(settings.TAG_PAGE_COUNTERS_PATH, str(self.__tag + ".txt"))
             self.__tag_run_counter_path = os.path.join(settings.TAG_RUN_COUNTER, str(self.__tag + ".txt"))
+
         self.__print()
-        self.__obtain_post_list()
+        self.__current_post_num = self.__obtain_cur_total_num_post()
+        print("Current posts available for the tag:", self.__current_post_num)
+
+        if self.__current_post_num == 0:
+            raise FalseTag("This tag does not exist")
+        else:
+            try:
+                with open(self.__tag_run_counter_path,'r') as f:
+                    run_time = int(float(f.read()))
+                with open(self.__tag_run_counter_path,'w') as f:
+                    f.write(str(run_time+1))
+            except FileNotFoundError:
+                run_time = 1
+                with open(self.__tag_run_counter_path,'w') as f:
+                    f.write(str(run_time+1))
+
+            if run_time == 1:
+                self.__obtain_post_list()
+            else:
+                self.__update_new_posts()
 
     def __print(self):
         '''
@@ -46,12 +66,57 @@ class DownloadDanbooruPics:
         print("~Welcome to the Danbooru Downloader")
         print("~Received tag:", self.__tag)
 
+
     def __obtain_access(self):
         '''
         Gets access to Danbooru
         :return: Access to Danbooru
         '''
         return Danbooru(site_name='danbooru')
+
+    def __obtain_post_list_per_page(self,page):
+        '''
+        Gets the list of post per page searched.
+        :param page: A page number
+        :return: A list of posts.
+        :type: list
+        '''
+        access = self.__obtain_access()
+        return access.post_list(page=page, tags=self.__tag)
+
+    def __obtain_post_amount(self,page):
+        '''
+        Gets the amount of posts for a page.
+        :param page: A searched page
+        :return: The number of posts in the page.
+        '''
+        post_list = self.__obtain_post_list_per_page(page)
+        if len(post_list) != 0:
+            return len(post_list)
+        else:
+            return 0
+
+    def __obtain_cur_total_num_post(self):
+        '''
+        Gets the current number of posts for a given tag
+        :return: The number of posts for a given tag
+        :type: int
+        '''
+        post_amount = 0
+        start = 1
+        cont_access = True
+        while cont_access:
+            try:
+                num_post = self.__obtain_post_amount(start)
+                if num_post == 0:
+                    cont_access = False
+            except KeyError:
+                cont_access = False
+            post_amount += num_post
+            if post_amount == 0:
+                cont_access = False
+            start += 1
+        return post_amount
 
     def __obtain_post_list(self):
         '''
@@ -115,7 +180,6 @@ class DownloadDanbooruPics:
             while that for Gold and Platinum users is 2000 and 5000, respectively.
 
         '''
-        access = self.__obtain_access()
         start = 1
         if os.path.isfile(self.__page_counter_path):
             with open(self.__page_counter_path, 'r') as current_counter:
@@ -126,15 +190,15 @@ class DownloadDanbooruPics:
                 new_counter.close()
         continue_access = True
         while continue_access:
-            print("Loading page", start)
+            print("Loading page",start)
             if start > ANON_SEARCH_LIMIT:
                 print("This is the limit to search for Anonymous users.\
                  To have more pages in search, please upgrade to Gold or Platinum.")
                 continue_access = False
             else:
                 try:
-                    post_list_obtained = access.post_list(page=start, tags = self.__tag)
-                    if len(post_list_obtained) != 0:
+                    post_list_obtained = self.__obtain_post_list_per_page(start)
+                    if self.__obtain_post_amount(start) != 0:
                         try:
                             cur_data = self.__load_pickle()
                             cur_data.extend(post_list_obtained)
@@ -150,8 +214,6 @@ class DownloadDanbooruPics:
                 req_write.close()
                 print("Loaded page",start)
                 start += 1
-        if os.stat(self.__pickle_path).st_size == 0:
-            print("There is no posts available for the given tag. Please try again.")
         os.remove(self.__page_counter_path)
 
     def __load_pickle(self):
@@ -161,6 +223,24 @@ class DownloadDanbooruPics:
         :type: list
         '''
         return DumpToPickle(self.__pickle_path).loadPickle()
+
+    def __update_new_posts(self):
+        '''
+        Updates the pickle with new posts.
+        '''
+        cur_data = self.__load_pickle()
+        num_new_posts = self.__current_post_num - len(cur_data)
+        print("There are",num_new_posts ,"new posts for this tag")
+        num_page = num_new_posts // MAX_POST_PER_PAGE + 1
+        remaining_new_posts = num_new_posts - MAX_POST_PER_PAGE * num_page
+        start = 1
+        while start <= num_page:
+            if start != num_page:
+                cur_data.extend(self.__obtain_post_list_per_page(start))
+            else:
+                cur_data.extend(self.__obtain_post_list_per_page(start)[:remaining_new_posts-1])
+            start += 1
+        DumpToPickle(self.__pickle_path,cur_data).DumpToPickle()
 
     def __obtain_post_ID(self, post):
         '''
@@ -266,12 +346,11 @@ class DownloadDanbooruPics:
         if len(url_list) != 0:
             id_list = self.__obtain_all_post_ID()
             ext_list = self.__obtain_all_file_ext()
-            download_base_location = "C:\\Users\\Hoang Tran\\Desktop\\Danbooru Collection"
             if self.__isAltered:
-                download_actual_location = download_base_location + "\\" + self.__temp_tag
+                download_actual_location = self.__location + "\\" + self.__temp_tag
                 file_counter_location = os.path.join(settings.TAG_COUNTERS_PATH, str(self.__temp_tag + "_file_counter.txt"))
             else:
-                download_actual_location = download_base_location + "\\" + self.__tag
+                download_actual_location = self.__location + "\\" + self.__tag
                 file_counter_location = os.path.join(settings.TAG_COUNTERS_PATH,
                                                      str(self.__tag + "_file_counter.txt"))
             if not os.path.isfile(file_counter_location) or os.stat(file_counter_location).st_size == 0:
@@ -299,16 +378,20 @@ class DownloadDanbooruPics:
                         print("Downloading:",url)
                         pic_name = str(id_list[link_pos]) + "." + ext_list[link_pos]
                         full_dir = download_actual_location + "\\" + pic_name
-                        req = urllib.request.Request(url,headers = {'User-Agent':
-                                                                        "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
-                                                                         AppleWebKit/537.36 (KHTML like Gecko) \
-                                                                         Chrome/51.0.2704.79 \
-                                                                         Safari/537.36 \
-                                                                         Edge/14.14931"})
-                        content = urllib.request.urlopen(req)
-                        with open(full_dir,'wb') as downloaded_pic:
-                            downloaded_pic.write(content.read())
-                        print("Finished downloading pic #" + str(link_pos + 1))
+                        try:
+                            if os.path.isfile(full_dir):
+                                print("Picture has previously been downloaded.")
+                                pass
+                        except FileNotFoundError:
+                            req = urllib.request.Request(url,headers = {'User-Agent':
+                                                                            "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+                                                                             AppleWebKit/537.36 (KHTML like Gecko) \
+                                                                             Chrome/51.0.2704.79 \
+                                                                             Safari/537.36 \
+                                                                             Edge/14.14931"})
+                            content = urllib.request.urlopen(req)
+                            with open(full_dir,'wb') as downloaded_pic:
+                                downloaded_pic.write(content.read())
                     else:
                         print("Pic #" + str(link_pos + 1) + " has no download link")
                     req_open = open(file_counter_location,'w')
@@ -325,15 +408,22 @@ class DownloadDanbooruPics:
         self.__download_pictures()
 
 class DownloadMultipleTags:
-    def __init__(self,tag_list):
+    def __init__(self,tag_list, location):
         self.__tag_list = tag_list
+        self.__location = location
 
     def __download_individual_tag(self, tag):
         '''
         Download the pictures of each tag
         :param tag: A tag that is listed as a tag in Danbooru
         '''
-        DownloadDanbooruPics(tag).initiate_download()
+        try:
+            DownloadDanbooruPics(tag,self.__location).initiate_download()
+        except FalseTag:
+            print("-------------------------------------------------------------------------------------------------------")
+            print()
+            print("Previous tag does not have any pictures.")
+            print()
 
     def __download_all_tags(self):
         '''
@@ -349,3 +439,12 @@ class DownloadMultipleTags:
         self.__download_all_tags()
 
 
+if __name__ == '__main__':
+    try:
+        DownloadDanbooruPics('nsnsnss',"prefered\\download\\location").initiate_download()
+    except FalseTag:
+        print()
+        print("-------------------------------------------------------------------------------------------------------")
+        print()
+        print("Previous tag does not have any pictures. Exception handled.")
+        DownloadDanbooruPics('sesshouin_kiara',"prefered\\download\\location").initiate_download()
