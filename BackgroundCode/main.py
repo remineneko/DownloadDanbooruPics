66b3bcade255ab38ea237b72551fb87b8d994a85
HoangTranUnion <56041330+HoangTranUnion@users.noreply.github.com>
2019-12-08 00:00:32 +0700

    Delete main.py

--- BackgroundCode/main.py
+++
@@ -1,9 +0,0 @@
-from DanbooruDownloader import start_download, isPath
-
-
-print('Welcome to Danbooru Downloader!')
-print('Please provide a path to the folder you want your pictures to be installed:')
-path = input()
-verified_path = isPath(path)
-start_download(verified_path)
-
